6a675b81182b08aac14fe4df9d78da2edfbdca83
DimovDimo <d.dimov1996@gmail.com>
2018-04-21 22:10:38 +0300

    added ShoppingList in 35.EXAM PREPARATION III

---
+++ 35.EXAM PREPARATION III/ShoppingList/Projects/Java/src/main/java/shoppinglist/entity/Product.java
@@ -0,0 +1,73 @@
+package shoppinglist.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String priority;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String quantity;
+
+    @Column(nullable = false)
+    private String status;
+
+    public Product() {
+    }
+
+    public Product(String priority, String name, String quantity, String status) {
+        this.priority = priority;
+        this.name = name;
+        this.quantity = quantity;
+        this.status = status;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
---
+++ 35.EXAM PREPARATION III/ShoppingList/Projects/JavaScript/models/product.js
@@ -0,0 +1,30 @@
+const Sequelize = require('sequelize');
+
+module.exports = function(sequelize){
+    const Product = sequelize.define('Product', {
+        priority: {
+            type: Sequelize.INTEGER,
+            allowNull: false,
+            required: true
+        },
+        name: {
+            type: Sequelize.TEXT,
+            allowNull: false,
+            required: true
+        },
+        quantity: {
+            type: Sequelize.INTEGER,
+            allowNull: false,
+            required: true
+        },
+        status: {
+            type: Sequelize.TEXT,
+            allowNull: false,
+            required: true
+        }
+    },{
+        timestamps:false
+    });
+
+    return Product;
+};
